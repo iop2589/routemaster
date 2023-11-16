6e7099dedc065b4d5132344eed21130714f03ef0
iop2589 <qkrtjdcjf07@naver.com>
2023-11-16 15:36:47 +0900

    Lombok dependency 수정 및 RouteInfo 경유지 추가

--- src/main/java/com/automation/route/routemaster/route/controller/RouteDataController.java
+++ src/main/java/com/automation/route/routemaster/route/controller/RouteDataController.java
@@ -59,7 +59,7 @@ public class RouteDataController {
 
   @ResponseBody
   @GetMapping("/naverRouteInfo")
-  public JSONObject getNaverRouteInfo (@RequestParam("start") String start, @RequestParam("goal") String goal, @RequestParam("waypoints") String wayPoints) {
+  public JSONObject getNaverRouteInfo (@RequestParam("start") String start, @RequestParam("goal") String goal) {
     JSONParser jsonParser = new JSONParser();
 
     ByteBuffer buffer = StandardCharsets.UTF_8.encode(start);
@@ -69,26 +69,55 @@ public class RouteDataController {
 
     URI uri;
 
-    if (wayPoints == null || wayPoints.isEmpty()) {
-      uri = UriComponentsBuilder.fromUriString(CommonUrl.NAVER.getUrl())
-                                    .path("/map-direction/v1/driving")
-                                    .queryParam("start", encodeStart)
-                                    .queryParam("goal", encodeGoal)
-                                    .encode()
-                                    .build()
-                                    .toUri();
-    } else {
-      buffer = StandardCharsets.UTF_8.encode(wayPoints);
-      String encodeWaypoints = StandardCharsets.UTF_8.decode(buffer).toString();
-      uri = UriComponentsBuilder.fromUriString(CommonUrl.NAVER.getUrl())
-                                    .path("/map-direction/v1/driving")
-                                    .queryParam("start", encodeStart)
-                                    .queryParam("goal", encodeGoal)
-                                    .queryParam("waypoints", encodeWaypoints)
-                                    .encode()
-                                    .build()
-                                    .toUri();
+    uri = UriComponentsBuilder.fromUriString(CommonUrl.NAVER.getUrl())
+                              .path("/map-direction/v1/driving")
+                              .queryParam("start", encodeStart)
+                              .queryParam("goal", encodeGoal)
+                              .encode()
+                              .build()
+                              .toUri();
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    RequestEntity<Void> req = RequestEntity.get(uri)
+                                          .header("X-NCP-APIGW-API-KEY-ID", CommonData.NAVERID)
+                                          .header("X-NCP-APIGW-API-KEY", CommonData.NAVERPW)
+                                          .build();
+
+    ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+    JSONObject obj = new JSONObject();
+
+    try {
+      obj = (JSONObject)jsonParser.parse( result.getBody() );
+    } catch (ParseException e) {
+      e.printStackTrace();
     }
+    return obj;
+  }
+
+  @ResponseBody
+  @GetMapping("/naverRouteInfoWayPoints")
+  public JSONObject getNaverRouteInfoWayPoints (@RequestParam("start") String start, @RequestParam("goal") String goal, @RequestParam("waypoints") String wayPoints) {
+    JSONParser jsonParser = new JSONParser();
+
+    ByteBuffer buffer = StandardCharsets.UTF_8.encode(start);
+    String encodeStart = StandardCharsets.UTF_8.decode(buffer).toString();
+    buffer = StandardCharsets.UTF_8.encode(goal);
+    String encodeGoal = StandardCharsets.UTF_8.decode(buffer).toString();
+
+    URI uri;
+
+    buffer = StandardCharsets.UTF_8.encode(wayPoints);
+    String encodeWaypoints = StandardCharsets.UTF_8.decode(buffer).toString();
+    uri = UriComponentsBuilder.fromUriString(CommonUrl.NAVER.getUrl())
+                              .path("/map-direction/v1/driving")
+                              .queryParam("start", encodeStart)
+                              .queryParam("goal", encodeGoal)
+                              .queryParam("waypoints", encodeWaypoints)
+                              .encode()
+                              .build()
+                              .toUri();
 
     RestTemplate restTemplate = new RestTemplate();
 
